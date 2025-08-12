624c93801571ffadfca24265e5486a3b3a3982bd
Priyanshu ranjan <priyanshuranjan0001@gmail.com>
2025-08-12 22:16:21 +0530

    Implement ERC20 operations: add transfer and burn functions, enhance ABI

--- src/tools/mantle/index.ts
+++ src/tools/mantle/index.ts
@@ -1,3 +1,4 @@
 export { getMntBalance } from "./getMntBalance";
 export { getErc20Balance } from "./getErc20Balance";
 export { deployContract } from "./deployContract";
+export { transferErc20, burnErc20 } from "./erc20Operations";
